a86f4cd9d68f5736407c7dcd0af4b26e5d08be01
ahmed elgendy <ahmedelgendy409@gmail.com>
2018-12-19 17:52:38 +0200

    home page ui

--- backend/assets/js/slideshow.js
+++ backend/assets/js/slideshow.js
@@ -8,7 +8,7 @@ function plusSlides(n) {
 
 function showSlides(n) {
   var i;
-  var slides = document.getElementsByClassName("sampleImage");
+  var slides = document.getElementsByClassName("game-wrapper");
   if (n > slides.length) {slideIndex = 1} 
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
