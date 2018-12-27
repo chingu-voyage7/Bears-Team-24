0aa298b6348cebd0f371942bf1bd0f9fd4014ab4
ahmed elgendy <ahmedelgendy409@gmail.com>
2018-12-27 23:54:55 +0200

    gitignore secret

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const mongoose = require('mongoose');
+const config = require('./config/secret');
 
-mongoose.connect('mongodb://127.0.0.1:27017', function(err) {
+mongoose.connect(config.database, function(err) {
   if (err) console.log(err);
   console.log("connected to the database");
 });
